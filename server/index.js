ae37afb4ae07f3e07270b432af0c7a59ce9922a6
Dmitry Murnaev <meow21ru@mail.ru>
2026-02-11 10:44:01 +0300

    Fix: deploid DB

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://finance-tracker-frontend-nxmx.onrender.com',
+        'https://finance-tracker-frontend.onrender.com'
+    ]
+}));
 app.use(express.json());
 
 // Подключение к PostgreSQL
