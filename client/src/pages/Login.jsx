c26b5fe5c177738540ad7b7d04d67ee1226779f1
Dmitry Murnaev <meow21ru@mail.ru>
2026-02-12 09:46:52 +0300

    Fix: styling login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
                     </h2>
                     <p className="text-center text-sm text-gray-600">
                         Или{' '}
-                        <Link to="/register" className="font-medium text-indigo-600 hover:text-indigo-500 transition">
+                        <Link to="/register" className="font-medium text-blue-600 hover:text-indigo-500 transition">
                             зарегистрируйтесь
                         </Link>
                     </p>
@@ -87,7 +87,7 @@ const Login = () => {
                         <button
                             type="submit"
                             disabled={loading}
-                            className={`group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-lg text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ${
+                            className={`group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-lg text-white bg-blue-600 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ${
                                 loading ? 'opacity-70 cursor-not-allowed' : ''
                             }`}
                         >
